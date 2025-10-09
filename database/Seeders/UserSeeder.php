7d3e1c5cc32fcfe47179660ca94d4633d6eaf2f5
Truonghai2 <truonghai16122002@gmail.com>
2025-10-09 17:27:10 +0700

    hotfix-userseed

--- database/Seeders/UserSeeder.php
+++ database/Seeders/UserSeeder.php
@@ -4,7 +4,6 @@ namespace Database\Seeders;
 
 use Core\Database\Seeder;
 use Core\Support\Facades\Hash;
-use Modules\User\Infrastructure\Models\Permission;
 use Modules\User\Infrastructure\Models\Role;
 use Modules\User\Infrastructure\Models\RoleAssignment;
 use Modules\User\Infrastructure\Models\User;
@@ -18,10 +17,6 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->command?->info('Syncing permissions from files...');
-        $this->syncPermissions();
-        $this->command?->info('Permissions synced.');
-
         $user = User::firstOrCreate(
             ['email' => 'admin@bault.dev'],
             [
@@ -30,44 +25,20 @@ class UserSeeder extends Seeder
             ],
         );
 
-        $this->command?->info('Admin user "admin@bault.dev" created or already exists.');
+        if ($user->wasRecentlyCreated) {
+            $this->command?->info('Admin user "admin@bault.dev" created.');
+        } else {
+            $this->command?->info('Admin user "admin@bault.dev" already exists.');
+        }
 
         $superAdminRole = Role::where('name', 'super-admin')->firstOrFail();
 
-        $allPermissionIds = Permission::all()->pluck('id')->all();
-        $superAdminRole->permissions()->sync($allPermissionIds);
-        $this->command?->info('Synced all permissions to super-admin role.');
-
-        RoleAssignment::updateOrCreate([
+        RoleAssignment::firstOrCreate([
             'user_id' => $user->id,
             'role_id' => $superAdminRole->id,
             'context_id' => 1,
         ]);
 
-        $this->command?->info('Ensured "admin@bault.dev" has the "super-admin" role.');
-    }
-
-    /**
-     * Synchronize permissions from module files to the database.
-     * This method ensures the permissions table is up-to-date with the code.
-     */
-    private function syncPermissions(): void
-    {
-        $filePermissions = [];
-        $moduleDirs = glob(base_path('Modules/*'), GLOB_ONLYDIR);
-
-        foreach ($moduleDirs as $dir) {
-            $permissionFile = $dir . '/permissions.php';
-            if (file_exists($permissionFile)) {
-                $modulePermissions = require $permissionFile;
-                if (is_array($modulePermissions)) {
-                    $filePermissions = array_merge($filePermissions, $modulePermissions);
-                }
-            }
-        }
-
-        foreach ($filePermissions as $name => $details) {
-            \Modules\User\Infrastructure\Models\Permission::updateOrCreate(['name' => $name], $details);
-        }
+        $this->command?->line('  - Ensured "admin@bault.dev" has the "super-admin" role in the system context.');
     }
 }
