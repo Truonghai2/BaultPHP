d4b065aade9b9b4b423286e4721e92ee8772b04a
haitt-php <haitt@hqgroups.vn>
2026-01-06 23:17:37 +0700

    fix tests

---
+++ tests/TestCase.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Tests;
+
+/**
+ * Alias for TestCases to maintain compatibility with existing tests.
+ * 
+ * @see TestCases
+ */
+abstract class TestCase extends TestCases
+{
+    // This class is just an alias for TestCases
+    // All functionality is inherited from TestCases
+}
+
